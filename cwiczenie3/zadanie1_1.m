f508781f36393707ff78486d44c4cd341c664b81
JW <55401412+kaoiks@users.noreply.github.com>
2024-03-14 17:33:42 +0100

    exercise 1_1

---
+++ cwiczenie3/zadanie1_1.m
@@ -0,0 +1,34 @@
+%%
+
+% Wczytanie danych
+load("Data01.mat")
+
+N = length(y);
+D = zeros(N-1, N);
+for i=1:(N-1)
+    D(i, i) = -1;
+    D(i, i+1) = 1;
+end
+q=1.5;
+size(D)
+%%
+cvx_begin
+    variable v(N)
+    
+    minimize(norm(y - v, 2) + norm(v(2:end)-v(1:end-1), 1) * q)
+    subject to 
+%         norm(D*v, 1) <= q
+%        norm(v(2:end)-v(1:end-1), 1) <= q;
+cvx_end
+%%
+N = length(y);
+x_values = linspace(0, 1, N);
+figure;
+scatter(t, y, "square", "filled", 'MarkerFaceColor', [0.5 0.5 0.5], 'MarkerEdgeColor', [0.5 0.5 0.5], 'LineWidth', 0.1);
+hold on;
+plot(t, v, 'LineWidth',2);
+hold off;
+xlabel('x/a');
+ylabel('y(x)');
+title('Optimized Variable y(x)');
+grid on;
